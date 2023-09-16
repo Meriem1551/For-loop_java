50ee4a420c4ebbe137c97f552abd93eac146baf9
Meriem1551 <meriem200409@gmail.com>
2023-09-16 18:08:55 +0200

    update project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -2,6 +2,7 @@ package org.math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0 ; i < 5 ; i++)
+            System.out.println("Hello World!" + i);//we can remove braces if we have onr line
     }
 }
